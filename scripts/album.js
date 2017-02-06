40eb4d930e282a7104b68e10d9eac6b15561479c
Chris Allen <ckallen12193@yahoo.com>
2017-02-06 09:24:12 -0800

    checkpoint 12

--- scripts/album.js
+++ scripts/album.js
@@ -28,10 +28,26 @@ var albumMarconi = {
      ]
  };
 
+var albumJazz = {
+     title: 'The very best of Benny Goodman',
+     artist: 'Benny Goodman',
+     label: 'Columbia',
+     year: '1937',
+     albumArtUrl: 'assets/images/album_covers/01.png',
+     songs: [
+         { title: 'Sweet Georgia Brown', duration: '3:07' },
+         { title: 'Bugie call rag', duration: '3:01' },
+         { title: 'St.Louis blues', duration: '3:23' },
+         { title: 'Swingtime in the rockies', duration: '3:10'},
+         { title: 'Exactly like you', duration: '3:19'}
+     ]
+ };
+
+
  var createSongRow = function(songNumber, songName, songLength) {
      var template =
         '<tr class="album-view-song-item">'
-      + '  <td class="song-item-number">' + songNumber + '</td>'
+      + '  <td class="song-item-number" data-song-number="' + songNumber + '">' + songNumber + '</td>'
       + '  <td class="song-item-title">' + songName + '</td>'
       + '  <td class="song-item-duration">' + songLength + '</td>'
       + '</tr>'
@@ -40,15 +56,15 @@ var albumMarconi = {
      return template;
  };
 
-var setCurrentAlbum = function(album) {
-     
-     var albumTitle = document.getElementsByClassName('album-view-title')[0];
+var albumTitle = document.getElementsByClassName('album-view-title')[0];
      var albumArtist = document.getElementsByClassName('album-view-artist')[0];
      var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
      var albumImage = document.getElementsByClassName('album-cover-art')[0];
      var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
  
-     
+
+var setCurrentAlbum = function(album) {
+    
      albumTitle.firstChild.nodeValue = album.title;
      albumArtist.firstChild.nodeValue = album.artist;
      albumReleaseInfo.firstChild.nodeValue = album.year + ' ' + album.label;
@@ -63,6 +79,40 @@ var setCurrentAlbum = function(album) {
      }
  };
  
+var songListContainer = document.getElementsByClassName('album-view-song-list')[0];
+var songRows = document.getElementsByClassName('album-view-song-item');
+
+var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
+
  window.onload = function() {
      setCurrentAlbum(albumPicasso);
+     
+     songListContainer.addEventListener('mouseover', function(event) {
+                if (event.target.parentElement.className === 'album-view-song-item') {
+             event.target.parentElement.querySelector('.song-item-number').innerHTML = playButtonTemplate;
+         }
+     });
+ 
+  for (var i = 0; i < songRows.length; i++) {
+         songRows[i].addEventListener('mouseleave', function(event) {
+         
+        
+             this.children[0].innerHTML = this.children[0].getAttribute('data-song-number');
+         });
+     }
+
+         });
+      
+     
+     
+     var albums = [albumPicasso, albumMarconi, albumJazz];
+     var index = 1;
+     albumImage.addEventListener("click", function(event) {
+         setCurrentAlbum(albums[index]);
+         index++;
+         if (index == albums.length) {
+             index = 0;
+         }
+         
+     });
  };
